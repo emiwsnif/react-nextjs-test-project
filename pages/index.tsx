c6d2c12e6a0ea7a59b77ab7077623068f09adccd
Andrii Rastorhuiev <emiwsnif@gmail.com>
2021-11-07 21:10:13 +0200

    added button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 import Image from 'next/image';
 import { Htag } from '../components';
+import { Button } from '../components/Button/Button';
 
 export default function Home(): JSX.Element {
     const a = 5;
     return (
         <div>
-            <Htag tag="h1">Text</Htag>
+            <Htag tag='h1'>Text</Htag>
+            <Button appearance='primary'>Button</Button>
+            <Button appearance='ghost'>Button</Button>
         </div>
     );
 }
