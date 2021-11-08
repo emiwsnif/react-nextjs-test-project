411e76086e6c66e551bec213d0713cb3be9dbb9c
Andrii Rastorhuiev <emiwsnif@gmail.com>
2021-11-08 23:14:25 +0200

    added component p

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,5 +1,5 @@
-import styles from './Button.module.css'
-import { ButtonProps } from './Button.props'
+import styles from './Button.module.css';
+import { ButtonProps } from './Button.props';
 import cn from 'classnames';
 
 export const Button = ({ appearance, arrow = 'none', children, className, ...props }: ButtonProps): JSX.Element => {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { Htag } from '../components';
 import { Button } from '../components/Button/Button';
+import { P } from '../components/P/P'
 
 export default function Home(): JSX.Element {
     const a = 5;
@@ -9,6 +10,9 @@ export default function Home(): JSX.Element {
             <Htag tag='h1'>Text</Htag>
             <Button appearance='primary' arrow='down'>Button</Button>
             <Button appearance='ghost' arrow='right'>Button</Button>
+            <P size='large'>Test</P>
+            <P size='medium'>Test</P>
+            <P size='small'>Test</P>
         </div>
     );
 }
