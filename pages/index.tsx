8365ba3aada591ae76fffdd7d99e2db4dac6b1fb
Andrii Rastorhuiev <emiwsnif@gmail.com>
2021-11-08 23:55:37 +0200

    added component tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import Image from 'next/image';
 import { Htag } from '../components';
 import { Button } from '../components/Button/Button';
-import { P } from '../components/P/P'
+import { P } from '../components/P/P';
+import { Tag } from '../components/Tag/Tag';
 
 export default function Home(): JSX.Element {
     const a = 5;
@@ -13,6 +13,12 @@ export default function Home(): JSX.Element {
             <P size='large'>Test</P>
             <P size='medium'>Test</P>
             <P size='small'>Test</P>
+            <Tag size='small'>Test</Tag>
+            <Tag size='medium'>Test</Tag>
+            <Tag size='small' color='red'>Test</Tag>
+            <Tag size='medium' color='red'>Test</Tag>
+            <Tag size='small' color='green'>Test</Tag>
+            <Tag size='small' color='primary'>Test</Tag>
         </div>
     );
 }
